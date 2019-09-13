7e13e21b4add5a869f1a6471f0defb2d401d7b61
Sonaligautam <sonaligautam10@gmail.com>
2019-09-13 18:53:52 +0100

    it is script file

---
+++ count.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+###########3   opertors  -gt -lt -le -ge 
+
+count=22
+
+if [ $count -gt 0 ] ;
+then
+echo "enter number is correct"
+printf "My current working directory is \n `pwd` \n "
+fi
